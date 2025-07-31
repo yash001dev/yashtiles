a6407c978aeb84501c9106e234f15d5f006435b2
BhumikP <bhumik.bm123@gmail.com>
2025-07-31 23:22:57 +0530

    remove from frame

--- app/frame/page.tsx
+++ app/frame/page.tsx
@@ -25,7 +25,6 @@ import {
 import { useFrameCustomizer } from "../../src/hooks/useFrameCustomizer";
 import { downloadFramedImage } from "../../src/utils/downloadImage";
 import HangBottomSheet from "../../src/components/HangBottomSheet";
-import FrameItHowItWorks from "../../src/components/dashboard/FrameItHowItWorks";
 
 function AppContent() {
   const { isAuthenticated } = useAuth();
@@ -337,13 +336,6 @@ function AppContent() {
         onOpenAuthModal={() => setIsAuthModalOpen(true)}
         cartTotal={calculateCartTotal}
       />
-
-      {/* How It Works Section */}
-      {!uploadedImage && (
-        <FrameItHowItWorks 
-          onDesignClick={() => document.querySelector('.photo-upload-area')?.scrollIntoView({ behavior: 'smooth' })}
-        />
-      )}
 
       {/* Content area starts after header */}
       <ResponsiveLayout
