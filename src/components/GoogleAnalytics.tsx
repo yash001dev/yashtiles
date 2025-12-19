8684d9247d6e848c18a63b6c6e8ff04d44b5919b
yash.sanghavi <yash.sanghavi@mordorintelligence.com>
2025-12-19 10:06:16 +0530

    fix: update GA measurement ID in deployment files and log it in GoogleAnalytics component

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@ import Script from 'next/script';
 
 export default function GoogleAnalytics() {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-
+  console.log("GA Measurement ID:", measurementId);
   if (!measurementId) {
     return null;
   }
